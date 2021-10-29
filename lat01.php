7ecec746f868f3c9f6c4ade7464e0f6743f0d82d
humanrizki <humanrizki123@gmail.com>
2021-10-29 10:02:19 +0700

    baru nulis tiga file

---
+++ lat01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Skrip PHP Pertama</title>
+</head>
+
+<body>
+<?php 
+     echo 'Ini Skrip PHP Pertamaku!'
+?>
+</body>
+
+</html>
---
+++ lat02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test Penyisipan PHP Pada HTML</title>
+</head>
+
+<body>
+<p>Kapal Asing, Silahkan identifikasi diri Anda</p><br>
+<?php 
+    // Berikut ini adalah kode PHP yang disisipkan
+     echo "<b>Ini adalah kapal Federasi Planet USS Enterprise.<br>
+     Saya Piccard, Jean Luc Piccard, Kapten Kapal
+       </b>";
+     
+?>
+</body>
+
+</html>
